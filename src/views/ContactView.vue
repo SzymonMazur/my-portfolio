1c0d823127f64a0ae7e1b1ae791bec6e437ae978
wagnam <szma001@student.kristiania.no>
2022-05-23 19:27:34 +0200

    added files

---
+++ src/views/ContactView.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="container">
+        <div class="row pt-5">
+            <article class="col-12 col-md-8">
+                
+                <img class="img-fluid mt-5" src="../assets/images/croods.png" alt="">
+            </article>
+            <article class="col-12 col-md-4 mt-5">
+                <h2 class="mb-5 fs-1" style="font-weight: 600;">Get in touch<span class="genie">🧞‍♂️</span></h2>
+                <h4 class="fs-5" style="font-weight: 400;">Email:</h4>
+                <a class="fs-4 nav-link p-0" href="mailto:szymon_98@icloud.com?Subject=Get%20In%20touch" target="_top" >szymon_98@icloud.com</a>
+                <h4 class="mt-5 fs-5" style="font-weight: 400;">By:
+                    <span class="pb-5">
+                        <a class="fs-4 mt-3 nav-link p-0" href="https://www.linkedin.com/in/szymon-mazur-07554b15b/">LinkedIn</a>
+                    </span>
+                    
+                    </h4>              
+        
+                
+                
+            </article>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.container{
+  font-family: Montserrat, sans-serif;
+}
+.genie{
+    font-size: 4rem;
+}
+.nav-link{
+    color: rgba(21, 49, 54, 1);
+    font-weight: 500;
+}
+
+.nav-link:hover{
+    color: #EFA90B;
+}
+.img{
+    height: 50rem;
+}
+</style>
